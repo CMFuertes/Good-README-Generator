83c8495ab5dbab511a543bfb8d4c9f9c7a6b58b8
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-01 13:19:14 -0400

    added api.js

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,17 @@
+const generateMarkdown = (obj) => {
+const readme = {
+  title : `# ${obj.title}\n`,
+}
+}
+
+
+// // function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+module.exports = {
+  generateMarkdown: generateMarkdown,
+}
